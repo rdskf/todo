7d0ab564f900c07ea4afbacc67128a75df2db55f
Crows Nest <millerrayan74@gmail.com>
2025-12-16 00:04:04 +0530

    todo app final

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,9 +3,11 @@ from todos.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
-    print(tasks)
+
+    completed_tasks = Task.objects.filter(is_completed = True)
+
     context = { 
-        'tasks' : tasks,
+        'tasks' : tasks, 
+        'completed_tasks' : completed_tasks,
     }
-    print(context)
     return render(request, 'home.html', context)
--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [ 
     path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name= 'mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name= 'mark_as_undone'),
+
+    # Edit Feature 
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
+
+    #Delete Task
+    path('delete_task/<int:pk>', views.delete_task, name='delete_task'),
 ]
